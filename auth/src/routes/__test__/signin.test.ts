bbcd76a0ef3dd23a92ac03dab5f4cf5d8c14dc67
Panca Sutresna <panca.sutresna@gmail.com>
2022-09-04 15:06:57 +0700

    fixing test script

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,42 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('return a 400 with an invalid email', async () => {
-    return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'testtest.com',
-            password: 'password',
-        })
-        .expect(400);
-});
-
-it('return a 400 with an invalid password', async () => {
-    return request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'p',
-        })
-        .expect(400);
-});
-
-it('return a 400 with missing email and password', async () => {
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-        })
-        .expect(400);
-
-    await request(app)
-        .post('/api/users/signup')
-        .send({
-            password: 'password',
-        })
-        .expect(400);
-});
-
 it('fails when a email that does not exist is supplied', async () => {
     await request(app)
         .post('/api/users/signin')
